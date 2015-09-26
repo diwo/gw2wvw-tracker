0249ebc1a2b81f5f1c0a432bebe394be925dbb80
Dickson Wong <dicksonwong@gmail.com>
2015-09-26 12:20:50 -0700

    Bootstrapped tests, added test for ajax module

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,27 @@
 var config = require('./package.json');
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var jasmine = require('gulp-jasmine');
 var nodemon = require('gulp-nodemon');
 
 var path = {
-  js: ['lib/**/*.js', 'gulpfile.js']
+  js: ['lib/**/*.js', 'gulpfile.js'],
+  tests: ['test/**/*.test.js']
 };
 
-gulp.task('default', ['lint']);
+gulp.task('default', ['test']);
+gulp.task('test', ['lint', 'jasmine']);
 
 gulp.task('lint', () =>
   gulp.src(path.js)
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(jshint.reporter('fail'))
+);
+
+gulp.task('jasmine', () =>
+  gulp.src(path.tests)
+    .pipe(jasmine())
 );
 
 gulp.task('nodemon', () => {
---
+++ test/ajax.test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var proxyquire = require('proxyquire');
+
+const URLs = {
+  success: 'success',
+  failure: 'failure'
+};
+
+const RESPONSE = {
+  success: {
+    body: 'body'
+  },
+  failure: {
+    error: Error('error')
+  }
+};
+
+var ajax = proxyquire('../lib/server/utils/ajax', {
+  request: function(url, cb) {
+    switch(url) {
+      case URLs.success:
+        setTimeout(() => { cb(null, {}, RESPONSE.success.body); }, 200);
+        break;
+      case URLs.failure:
+        setTimeout(() => { cb(RESPONSE.failure.error); }, 200);
+        break;
+      default:
+        throw Error('Unhandled test case');
+    }
+  }
+});
+
+describe('ajax()', () => {
+  it('Resolves to response body when request is successful', done => {
+    ajax(URLs.success)
+      .then(
+        body => { expect(body).toBe(RESPONSE.success.body); },
+        error => { expect(error).toBeUndefined(); }
+      )
+      .then(done);
+  });
+
+  it('Rejects with error when request is failure', done => {
+    ajax(URLs.failure)
+      .then(
+        () => { done.fail('ajax() resolved instead of reject'); },
+        error => { expect(error).toBe(RESPONSE.failure.error); }
+      )
+      .then(done);
+  });
+});
