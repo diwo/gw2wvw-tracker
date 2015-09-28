c66c37c0f79310d00b10376671e3b482c49d69f1
Dickson Wong <dicksonwong@gmail.com>
2015-09-27 23:58:51 -0700

    Fixed match_details_store.getLast(...) to get last for a match id

--- test/itest/match_details_store.itest.js
+++ test/itest/match_details_store.itest.js
@@ -4,11 +4,11 @@ var match_details_store = require('../../lib/server/persistence/match_details_st
 
 describe('match details add and getLast', () => {
   it('getLast resolves to last match detail added', done => {
-    match_details_store.add({ n: 1 })
-      .then(() => match_details_store.add({ n: 2 }))
-      .then(() => match_details_store.add({ n: 3 }))
-      .then(() => match_details_store.getLast())
-      .then(details => { expect(details.n).toBe(3); })
+    match_details_store.add({ 'match_id': 1, n: 1 })
+      .then(() => match_details_store.add({ 'match_id': 1, n: 2 }))
+      .then(() => match_details_store.add({ 'match_id': 2, n: 3 }))
+      .then(() => match_details_store.getLast(1))
+      .then(details => { expect(details.n).toBe(2); })
       .then(
         () => { done(); },
         err => { done.fail(err); }
